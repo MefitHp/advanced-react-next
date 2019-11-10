f431a90a42c891d2d9367e29defe3b169263a079
MefitHp <mefitdev@gmail.com>
2019-11-09 22:58:57 -0600

    Deployed prisma server

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,16 +1,16 @@
 import React from "react";
-import App, { Container } from "next/app";
+import App from "next/app";
 import Layout from "../components/Layout";
 
 export default class MyApp extends App {
   render() {
     const { Component } = this.props;
     return (
-      <Container>
+      <>
         <Layout>
           <Component />
         </Layout>
-      </Container>
+      </>
     );
   }
 }
