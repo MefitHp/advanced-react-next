590f6049cadf3e7a679d9c81e22a8ad4aa19899b
MefitHp <mefitdev@gmail.com>
2019-11-15 10:10:57 -0600

    Added backend setup

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+// Those are like Resolvers for the actions made
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  items: forwardTo("db")
+  //   async items(parent, args, ctx, info) {
+  //     const items = await ctx.db.query.items();
+  //     return items;
+  //   }
+};
 
 module.exports = Query;
