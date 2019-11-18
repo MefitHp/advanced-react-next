6a44536ce02846949b945b2662ae579f9b6281a7
MefitHp <mefitdev@gmail.com>
2019-11-17 20:31:11 -0600

    Added mutatuons/resolver to get/update one item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,8 +1,16 @@
 const mutations = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
-
     return item;
+  },
+  async updateItem(parent, args, ctx, info) {
+    const valuesToUpdate = { ...args };
+    delete valuesToUpdate.id;
+
+    return ctx.db.mutation.updateItem({
+      data: valuesToUpdate,
+      where: { id: args.id }
+    });
   }
 };
 
